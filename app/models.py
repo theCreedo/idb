6661ea4dc63a55fad218c6d82013715c8ddc389e
Nandhini Lakuduva <Nandhini@Nandhinis-MacBook-Pro.local>
2017-03-23 19:47:02 -0500

    Closes #12: added __init__ implementations and assertions to models.py

--- app/models.py
+++ app/models.py
@@ -29,6 +29,29 @@ class Track(Base) :
 	album_id = Column(Integer, ForeignKey('albums.id'))
 	album = relationship('Album', back_populates='tracks')
 
+	def __init__(self, name, genre, release_date, duration, 
+		popularity, preview_url, explicit, artist_id, album_id):
+		assert (name != "")
+		assert (genre != "")
+		assert (release_date != "")
+		assert (duration > 0)
+		assert (popularity >= 0 and popularity <= 100)
+		assert (preview_url != "")
+		assert (explicit != "")
+		assert (artist_id > 0)
+		assert (album_id > 0)
+
+        self.name = name
+        self.genre = genre
+        self.release_date = release_date
+        self.duration = duration
+        self.popularity = popularity
+        self.preview_url = preview_url
+        self.explicit = explicit
+        self.artist_id = artist_id
+        self.album_id = album_id
+
+
 	def __repr__(self) :
 		return "<Track(name='%s', artist='%s')>" % (self.name, self.artist.name)
 
@@ -48,6 +71,13 @@ class Artist_Album_Association(Base) :
 	# Auto-populates Album.artists
 	album_id = Column(Integer, ForeignKey('albums.id'))
 	album = relationship('Album', back_populates='artists')
+
+	def __init__(artist_id, album_id):
+		assert (artist_id > 0)
+		assert (album_id > 0)
+
+        self.artist_id = artist_id
+        self.album_id = album_id
 
 	def __repr__(self) :
 		return "<Artist_Album_Association(artist='%s', album='%s')>" %\
@@ -77,6 +107,21 @@ class Artist(Base) :
 	tracks = relationship('Track', 
 		order_by=Track.id, back_populates='artist')
 
+	def __init__(self, name, image_url, country, decade, genre, most_popular_track_id):
+		assert (name != "")
+		assert (image_url != "")
+		assert (country != "")
+		assert (decade != "")
+		assert (genre != "")
+		assert (most_popular_track > 0)
+
+        self.name = name
+        self.image_url = image_url
+        self.country = self.country
+        self.decade = self.decade
+        self.genre = genre
+        self.most_popular_track_id = most_popular_track_id
+
 	def __repr__(self) :
 		return "<Artist(name='%s')>" % (self.name)
 
@@ -101,6 +146,21 @@ class Album(Base) :
 	tracks = relationship('Track', 
 		order_by=Track.id, back_populates='album')
 
+	def __init__(self, name, genre, release_date, album_cover_url, label, number_of_tracks):
+		assert (name != "")
+		assert (genre != "")
+		assert (release_date != "")
+		assert (album_cover_url != "")
+		assert (label != "")
+		assert (number_of_tracks > 0)
+
+        self.name = name
+        self.genre = genre
+        self.release_date = release_date
+        self.album_cover_url = album_cover_url
+        self.label = label
+        self.number_of_tracks = number_of_tracks
+
 	def __repr__(self) :
 		return "<Album(name='%s', label=%s)>" % (self.name, self.label)
 
@@ -120,6 +180,15 @@ class Concert_AA_Association(Base) :
 	# Reference to artist_album_pairs table
 	aa_id = Column(Integer, ForeignKey('artist_album_pairs.id'))
 	artist_album = relationship('Artist_Album_Association')
+
+
+	def __init__(concert_id, aa_id):
+		assert (concert_id > 0)
+		assert (aa_id > 0)
+
+        self.concert_id = concert_id
+        self.aa_id = aa_id
+
 
 	def __repr__(self) :
 		return "<Concert_AA_Association(concert='%s', artist='%s', album='%s'\
@@ -145,6 +214,21 @@ class Concert(Base) :
 	artist_album_pairs = relationship('Concert_AA_Association',
 		order_by=Concert_AA_Association.id, back_populates='concert')
 
+	def __init__(self, name, event_link, date, time, venue_id):
+		assert (name != "")
+		assert (genre != "")
+		assert (event_link != "")
+		assert (date != "")
+		assert (time != "")
+		assert (venue_id > 0)
+
+        self.name = name
+        self.genre = genre
+        self.event_link = event_link
+        self.date = date
+        self.time = time
+        self.venue_id = venue_id
+
 	def __repr__(self) :
 		return "<Concert(name='%s')>" % (self.name)
 
@@ -163,6 +247,23 @@ class Venue(Base) :
 	# Relationship with concerts table
 	# Auto-populates Concert.venue
 	concerts = relationship('Concert', back_populates='venue')
+
+	def __init__(self, name, image_url, city, region, country, latitude, longitude):
+		assert (name != "")
+		assert (image_url != "")
+		assert (city != "")
+		assert (region != "")
+		assert (country != "")
+		assert (latitude >= 0.0)
+		assert (longitude >= 0.0)
+
+        self.name = name
+        self.image_url = image_url
+        self.city = city
+        self.region = region
+        self.country = country
+        self.latitude = latitude
+        self.longitude = longitude
 
 	def __repr__(self) :
 		return "<Venue(name='%s', city='%s')>" % (self.name, self.city)
