fec44464f2f3e2e60940da846f11cde0bfc5fa6b
Benjamin Rodgers <brodgers@cs.utexas.edu>
2017-03-30 17:21:59 -0500

    added postgres connection models.py, reformatted models.py and tests.py with make format

--- app/models.py
+++ app/models.py
@@ -1,45 +1,46 @@
 #! python
 from sqlalchemy import create_engine, Column, ForeignKey,\
-	Integer, Boolean, Table, Float, String
+    Integer, Boolean, Table, Float, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLAlchemy Declarative Base
 Base = declarative_base()
 
-class Track(Base) :
-	__tablename__ = 'tracks'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(50))
-	genre = Column(String(25))
-	release_date = Column(String(15))
-	duration = Column(Integer)
-	popularity = Column(Integer)
-	preview_url = Column(String(150))
-	explicit = Column(Boolean)
 
-	# Reference to artists table 
-	# Auto-populates Artist.tracks
-	artist_id = Column(Integer, ForeignKey('artists.id'))
-	artist = relationship('Artist', back_populates='tracks')
-	
-	# Reference to albums table
-	# Auto-populates Album.tracks
-	album_id = Column(Integer, ForeignKey('albums.id'))
-	album = relationship('Album', back_populates='tracks')
+class Track(Base):
+    __tablename__ = 'tracks'
 
-	def __init__(self, name, genre, release_date, duration, 
-		popularity, preview_url, explicit, artist_id, album_id):
-		assert (name != "")
-		assert (genre != "")
-		assert (release_date != "")
-		assert (duration > 0)
-		assert (popularity >= 0 and popularity <= 100)
-		assert (preview_url != "")
-		assert (explicit != "")
-		assert (artist_id > 0)
-		assert (album_id > 0)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    genre = Column(String(25))
+    release_date = Column(String(15))
+    duration = Column(Integer)
+    popularity = Column(Integer)
+    preview_url = Column(String(150))
+    explicit = Column(Boolean)
+
+    # Reference to artists table
+    # Auto-populates Artist.tracks
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+    artist = relationship('Artist', back_populates='tracks')
+
+    # Reference to albums table
+    # Auto-populates Album.tracks
+    album_id = Column(Integer, ForeignKey('albums.id'))
+    album = relationship('Album', back_populates='tracks')
+
+    def __init__(self, name, genre, release_date, duration,
+                 popularity, preview_url, explicit, artist_id, album_id):
+        assert (name != "")
+        assert (genre != "")
+        assert (release_date != "")
+        assert (duration > 0)
+        assert (popularity >= 0 and popularity <= 100)
+        assert (preview_url != "")
+        assert (explicit != "")
+        assert (artist_id > 0)
+        assert (album_id > 0)
 
         self.name = name
         self.genre = genre
@@ -51,69 +52,71 @@ class Track(Base) :
         self.artist_id = artist_id
         self.album_id = album_id
 
+    def __repr__(self):
+        return "<Track(name='%s', artist='%s')>" % (self.name, self.artist.name)
 
-	def __repr__(self) :
-		return "<Track(name='%s', artist='%s')>" % (self.name, self.artist.name)
-
-# Association Class to model the many-to-many relationship between 
+# Association Class to model the many-to-many relationship between
 # Artists and Albums
-class Artist_Album_Association(Base) :
-	__tablename__ = 'artist_album_pairs'
 
-	id = Column(Integer, primary_key=True)
 
-	# Reference to artists table
-	# Auto-populates Artist.albums
-	artist_id = Column(Integer, ForeignKey('artists.id'))
-	artist = relationship('Artist', back_populates='albums')
+class Artist_Album_Association(Base):
+    __tablename__ = 'artist_album_pairs'
 
-	# Reference to albums table
-	# Auto-populates Album.artists
-	album_id = Column(Integer, ForeignKey('albums.id'))
-	album = relationship('Album', back_populates='artists')
+    id = Column(Integer, primary_key=True)
 
-	def __init__(artist_id, album_id):
-		assert (artist_id > 0)
-		assert (album_id > 0)
+    # Reference to artists table
+    # Auto-populates Artist.albums
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+    artist = relationship('Artist', back_populates='albums')
+
+    # Reference to albums table
+    # Auto-populates Album.artists
+    album_id = Column(Integer, ForeignKey('albums.id'))
+    album = relationship('Album', back_populates='artists')
+
+    def __init__(artist_id, album_id):
+        assert (artist_id > 0)
+        assert (album_id > 0)
 
         self.artist_id = artist_id
         self.album_id = album_id
 
-	def __repr__(self) :
-		return "<Artist_Album_Association(artist='%s', album='%s')>" %\
-			(self.artist.name, self.album.name)
+    def __repr__(self):
+        return "<Artist_Album_Association(artist='%s', album='%s')>" %\
+            (self.artist.name, self.album.name)
 
-class Artist(Base) :
-	__tablename__ = 'artists'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(50))
-	image_url = Column(String(150))
-	country = Column(String(50))
-	decade = Column(String(50))
-	genre = Column(String(100))
+class Artist(Base):
+    __tablename__ = 'artists'
 
-	# Reference to an artist's most popular track
-	most_popular_track_id = Column(Integer, ForeignKey('tracks.id'))
-	most_popular_track = relationship('Track')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    image_url = Column(String(150))
+    country = Column(String(50))
+    decade = Column(String(50))
+    genre = Column(String(100))
 
-	# Relationship to artist_album_pairs table 
-	# Auto-populates Artist_Album_Association.artist
-	albums = relationship('Artist_Album_Association', 
-		order_by=Artist_Album_Association.id, back_populates='artist')
+    # Reference to an artist's most popular track
+    most_popular_track_id = Column(Integer, ForeignKey('tracks.id'))
+    most_popular_track = relationship('Track')
 
-	# Relationship to tracks table
-	# Auto-populates Track.artist
-	tracks = relationship('Track', 
-		order_by=Track.id, back_populates='artist')
+    # Relationship to artist_album_pairs table
+    # Auto-populates Artist_Album_Association.artist
+    albums = relationship('Artist_Album_Association',
+                          order_by=Artist_Album_Association.id, back_populates='artist')
 
-	def __init__(self, name, image_url, country, decade, genre, most_popular_track_id):
-		assert (name != "")
-		assert (image_url != "")
-		assert (country != "")
-		assert (decade != "")
-		assert (genre != "")
-		assert (most_popular_track > 0)
+    # Relationship to tracks table
+    # Auto-populates Track.artist
+    tracks = relationship('Track',
+                          order_by=Track.id, back_populates='artist')
+
+    def __init__(self, name, image_url, country, decade, genre, most_popular_track_id):
+        assert (name != "")
+        assert (image_url != "")
+        assert (country != "")
+        assert (decade != "")
+        assert (genre != "")
+        assert (most_popular_track > 0)
 
         self.name = name
         self.image_url = image_url
@@ -122,37 +125,38 @@ class Artist(Base) :
         self.genre = genre
         self.most_popular_track_id = most_popular_track_id
 
-	def __repr__(self) :
-		return "<Artist(name='%s')>" % (self.name)
+    def __repr__(self):
+        return "<Artist(name='%s')>" % (self.name)
 
-class Album(Base) :
-	__tablename__ = 'albums'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(50))
-	genre = Column(String(100))
-	release_date = Column(String(15))
-	album_cover_url = Column(String(150))
-	label = Column(String(50))
-	number_of_tracks = Column(Integer)
+class Album(Base):
+    __tablename__ = 'albums'
 
-	# Relationship to artist_album_pairs table
-	# Auto-populates Artist_Album_Association.album
-	artists = relationship('Artist_Album_Association',
-		order_by=Artist_Album_Association.id, back_populates='album')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    genre = Column(String(100))
+    release_date = Column(String(15))
+    album_cover_url = Column(String(150))
+    label = Column(String(50))
+    number_of_tracks = Column(Integer)
 
-	# Relationship to tracks table
-	# Auto-populates Track.album
-	tracks = relationship('Track', 
-		order_by=Track.id, back_populates='album')
+    # Relationship to artist_album_pairs table
+    # Auto-populates Artist_Album_Association.album
+    artists = relationship('Artist_Album_Association',
+                           order_by=Artist_Album_Association.id, back_populates='album')
 
-	def __init__(self, name, genre, release_date, album_cover_url, label, number_of_tracks):
-		assert (name != "")
-		assert (genre != "")
-		assert (release_date != "")
-		assert (album_cover_url != "")
-		assert (label != "")
-		assert (number_of_tracks > 0)
+    # Relationship to tracks table
+    # Auto-populates Track.album
+    tracks = relationship('Track',
+                          order_by=Track.id, back_populates='album')
+
+    def __init__(self, name, genre, release_date, album_cover_url, label, number_of_tracks):
+        assert (name != "")
+        assert (genre != "")
+        assert (release_date != "")
+        assert (album_cover_url != "")
+        assert (label != "")
+        assert (number_of_tracks > 0)
 
         self.name = name
         self.genre = genre
@@ -161,66 +165,67 @@ class Album(Base) :
         self.label = label
         self.number_of_tracks = number_of_tracks
 
-	def __repr__(self) :
-		return "<Album(name='%s', label=%s)>" % (self.name, self.label)
+    def __repr__(self):
+        return "<Album(name='%s', label=%s)>" % (self.name, self.label)
 
 # Association class to model the many-to-many relationship between
 # Concerts and Artists and Albums
-class Concert_AA_Association(Base) :
-	__tablename__ = 'concert_aa_pairs'
-
-	id = Column(Integer, primary_key=True)
-
-	# Reference to concerts table
-	# Auto-populates Concert.artist_album_pairs
-	concert_id = Column(Integer, ForeignKey('concerts.id'))
-	concert = relationship('Concert', 
-		back_populates='artist_album_pairs')
-
-	# Reference to artist_album_pairs table
-	aa_id = Column(Integer, ForeignKey('artist_album_pairs.id'))
-	artist_album = relationship('Artist_Album_Association')
 
 
-	def __init__(concert_id, aa_id):
-		assert (concert_id > 0)
-		assert (aa_id > 0)
+class Concert_AA_Association(Base):
+    __tablename__ = 'concert_aa_pairs'
+
+    id = Column(Integer, primary_key=True)
+
+    # Reference to concerts table
+    # Auto-populates Concert.artist_album_pairs
+    concert_id = Column(Integer, ForeignKey('concerts.id'))
+    concert = relationship('Concert',
+                           back_populates='artist_album_pairs')
+
+    # Reference to artist_album_pairs table
+    aa_id = Column(Integer, ForeignKey('artist_album_pairs.id'))
+    artist_album = relationship('Artist_Album_Association')
+
+    def __init__(concert_id, aa_id):
+        assert (concert_id > 0)
+        assert (aa_id > 0)
 
         self.concert_id = concert_id
         self.aa_id = aa_id
 
+    def __repr__(self):
+        return "<Concert_AA_Association(concert='%s', artist='%s', album='%s'\
+            )>" % (self.concert.name, self.artist_album.artist.name,
+            self.artist_album.album.name)
 
-	def __repr__(self) :
-		return "<Concert_AA_Association(concert='%s', artist='%s', album='%s'\
-			)>" % (self.concert.name, self.artist_album.artist.name,
-			self.artist_album.album.name)
 
-class Concert(Base) :
-	__tablename__ = 'concerts'
+class Concert(Base):
+    __tablename__ = 'concerts'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(50))
-	event_link = Column(String(150))
-	date = Column(String(15))
-	time = Column(String(10))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    event_link = Column(String(150))
+    date = Column(String(15))
+    time = Column(String(10))
 
-	# Reference to venues table
-	# Auto-populates Venue.concerts
-	venue_id = Column(Integer, ForeignKey('venues.id'))
-	venue = relationship('Venue', back_populates='concerts')
+    # Reference to venues table
+    # Auto-populates Venue.concerts
+    venue_id = Column(Integer, ForeignKey('venues.id'))
+    venue = relationship('Venue', back_populates='concerts')
 
-	# Relationship to artist_album_pairs table
-	# Auto-populates Concert_AA_Association.concert
-	artist_album_pairs = relationship('Concert_AA_Association',
-		order_by=Concert_AA_Association.id, back_populates='concert')
+    # Relationship to artist_album_pairs table
+    # Auto-populates Concert_AA_Association.concert
+    artist_album_pairs = relationship('Concert_AA_Association',
+                                      order_by=Concert_AA_Association.id, back_populates='concert')
 
-	def __init__(self, name, event_link, date, time, venue_id):
-		assert (name != "")
-		assert (genre != "")
-		assert (event_link != "")
-		assert (date != "")
-		assert (time != "")
-		assert (venue_id > 0)
+    def __init__(self, name, event_link, date, time, venue_id):
+        assert (name != "")
+        assert (genre != "")
+        assert (event_link != "")
+        assert (date != "")
+        assert (time != "")
+        assert (venue_id > 0)
 
         self.name = name
         self.genre = genre
@@ -229,33 +234,34 @@ class Concert(Base) :
         self.time = time
         self.venue_id = venue_id
 
-	def __repr__(self) :
-		return "<Concert(name='%s')>" % (self.name)
+    def __repr__(self):
+        return "<Concert(name='%s')>" % (self.name)
 
-class Venue(Base) :
-	__tablename__ = 'venues'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(50))
-	image_url = Column(String(150))
-	city = Column(String(50))
-	region = Column(String(50))
-	country = Column(String(50))
-	latitude = Column(Float)
-	longitude = Column(Float)
+class Venue(Base):
+    __tablename__ = 'venues'
 
-	# Relationship with concerts table
-	# Auto-populates Concert.venue
-	concerts = relationship('Concert', back_populates='venue')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    image_url = Column(String(150))
+    city = Column(String(50))
+    region = Column(String(50))
+    country = Column(String(50))
+    latitude = Column(Float)
+    longitude = Column(Float)
 
-	def __init__(self, name, image_url, city, region, country, latitude, longitude):
-		assert (name != "")
-		assert (image_url != "")
-		assert (city != "")
-		assert (region != "")
-		assert (country != "")
-		assert (latitude >= 0.0)
-		assert (longitude >= 0.0)
+    # Relationship with concerts table
+    # Auto-populates Concert.venue
+    concerts = relationship('Concert', back_populates='venue')
+
+    def __init__(self, name, image_url, city, region, country, latitude, longitude):
+        assert (name != "")
+        assert (image_url != "")
+        assert (city != "")
+        assert (region != "")
+        assert (country != "")
+        assert (latitude >= 0.0)
+        assert (longitude >= 0.0)
 
         self.name = name
         self.image_url = image_url
@@ -265,14 +271,13 @@ class Venue(Base) :
         self.latitude = latitude
         self.longitude = longitude
 
-	def __repr__(self) :
-		return "<Venue(name='%s', city='%s')>" % (self.name, self.city)
+    def __repr__(self):
+        return "<Venue(name='%s', city='%s')>" % (self.name, self.city)
 
 # SQLAlchemy's connection to database
 # See SQLAlchemy's documation on PostgreSQL format
-# Commented out for Phase 1 
-# engine = create_engine('POSTGRES_DB_HERE')
+# Commented out for Phase 1
+engine = create_engine('postgres://postgres:swe2017@localhost:5432/postgres')
 
 # Create tables in database from classes
-# Base.metadata.create_all(engine)
-
+Base.metadata.create_all(engine)
--- app/tests.py
+++ app/tests.py
@@ -9,12 +9,14 @@ from application import app
 
 
 class TestCase(unittest.TestCase):
+
     def setUp(self):
         # Flask apps testing. See: http://flask.pocoo.org/docs/testing/
         app.config['TESTING'] = True
         app.config['CSRF_ENABLED'] = False
         self.app = app.test_client()
-        # Setups app engine test bed. See: http://code.google.com/appengine/docs/python/tools/localunittesting.html#Introducing_the_Python_Testing_Utilities
+        # Setups app engine test bed. See:
+        # http://code.google.com/appengine/docs/python/tools/localunittesting.html#Introducing_the_Python_Testing_Utilities
         self.testbed = testbed.Testbed()
         self.testbed.activate()
         self.testbed.init_datastore_v3_stub()
@@ -24,26 +26,26 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         self.testbed.deactivate()
 
-    #Test to see if going to the main URL actually works
+    # Test to see if going to the main URL actually works
     def test_home_redirects(self):
         rv = self.app.get('/')
         assert rv.status == '302 FOUND'
 
-    #Tests to see if boswemianrhapsody.me and boswemianrhapsody.me/home.html
-    #are the same page
+    # Tests to see if boswemianrhapsody.me and boswemianrhapsody.me/home.html
+    # are the same page
     def test_home_default(self):
         rv = self.app.get('/')
         rv1 = self.app.get('/home.html')
 
         assert rv.data == rv1.data
 
-    #Tests to see if routing to about page works
+    # Tests to see if routing to about page works
     def test_about_redirect(self):
         rv = self.app.get('/about.html')
         assert rv.status == '302 FOUND'
         assert 'Meet BoSWEmian Rhapsody' in rv.data
 
-    #Tests to see if all the tables are accessible
+    # Tests to see if all the tables are accessible
     def test_table_redirect(self):
         rv = self.app.get('/tracksTable.html')
         assert rv.status == '302 FOUND'
@@ -61,7 +63,7 @@ class TestCase(unittest.TestCase):
         assert rv.status == '302 FOUND'
         assert 'Concerts Table' in rv.data
 
-    #Tests to see if posting a new album to the album table works
+    # Tests to see if posting a new album to the album table works
     def test_post_album(self):
         rv = self.app.post('/albumsTable.html', data=dict(
             name='A Head Full of Dreams'
@@ -72,26 +74,25 @@ class TestCase(unittest.TestCase):
         assert 'No albums' not in rv.data
         assert 'A Head Full of Dreams' in rv.data
 
-    #Tests to see if the posted album can be modified
+    # Tests to see if the posted album can be modified
     def test_put_album(self):
         test_post_album()
         rv = self.app.put('/albumsTable.html', data=dict(
-                id=2000
+            id=2000
                 name='A Head Full Of Dreams'
-            ), follow_redirects=True)
+        ), follow_redirects=True)
         assert rv.status == '200 SUCCESS'
         assert 'A Head Full Of Dreams' in rv.data
 
-    #Tests to see if it is possible to go to the album page after
-    #it has been posted
+    # Tests to see if it is possible to go to the album page after
+    # it has been posted
     def test_album_table_redirect(self):
         test_post_album()
         rv = self.app.get('/coldplayAlbumPage.html')
         assert rv.status == '302 FOUND'
         assert 'A Head Full of Dreams' in rv.data
 
-
-    #Tests posting a track to the track table.
+    # Tests posting a track to the track table.
     def test_post_track(self):
         rv = self.app.post('/tracksTable.html', data=dict(
             name='Adventure of a Lifetime',
@@ -103,19 +104,19 @@ class TestCase(unittest.TestCase):
         assert 'No tracks' not in rv.data
         assert 'Adventure of a Lifetime' in rv.data
 
-    #Tests modifying a posted track. Documentation will show that
-    #this is different from modifying an album since a track requires
-    #both a name AND an ID, rather than just and ID.
+    # Tests modifying a posted track. Documentation will show that
+    # this is different from modifying an album since a track requires
+    # both a name AND an ID, rather than just and ID.
     def test_put_track(self):
         rv = self.app.put('/tracksTable.html', data=dict(
-                id=2000
+            id=2000
                 name='Adventure Of A Lifetime'
-            ), follow_redirects=True)
+        ), follow_redirects=True)
 
         assert rv.status == '200 SUCCESS'
         assert 'Adventure Of A Lifetime' in rv.data
 
-    #Tests 404 page.
+    # Tests 404 page.
     def test_404(self):
         rv = self.app.get('/missing')
         assert rv.status == '404 NOT FOUND'
