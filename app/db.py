88cd310b6bac4aa505db6f2483a50f04c23e68fd
Jerry Sun <Jerry@jerrys-MBP.lan>
2017-04-02 17:39:10 -0500

    Trying to establish aa_associations and c_aa associations

--- app/db.py
+++ app/db.py
@@ -5,16 +5,22 @@ track = Track('name', 'genre', 'release', 1, 1, 'url', True)
 artist = Artist('name', 'url', 'country', 'decade', 'genre')
 album = Album('name', 'genre', 'release', 'url', 'label')
 aa = new Artist_Album_Association()
-
+c_aa = new Concert_AA_Association()
 
 artist.tracks.append(track)
 album.tracks.append(track)
-album.albums = aa
+
+# instead of adding the album directly, it will add an aa
+artist.albums.append(aa)
+# instead of adding the artist directly, it will add an aa 
+album.artists.append(aa)
+
 
 concert = Concert('name', 'link', 'date', 'time')
 venue = Venue('name', 'url', 'city', 'region', 'country', 0.0, 0.0)
 
 venue.concerts.append(concert)
+concert.artist_album_pairs.append(c_aa)
 
 db.session.add(track)
 db.session.add(artist)
