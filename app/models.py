8a8ea07327aaa4e4b4724b3bf5fb8f1a247d450a
brodgers <rodgers.benjamin@utexas.edu>
2017-04-05 10:05:14 -0500

    increased lengths of strings to support longer input, added (commented out) code to drop tables if necessary

--- app/models.py
+++ app/models.py
@@ -12,12 +12,12 @@ class Track(db.Model):
     __tablename__ = 'tracks'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(150))
     genre = db.Column(db.String(25))
     release_date = db.Column(db.String(15))
     duration = db.Column(db.Integer)
     popularity = db.Column(db.Integer)
-    preview_url = db.Column(db.String(150))
+    preview_url = db.Column(db.String(200))
     explicit = db.Column(db.Boolean)
 
     # Reference to artists table
@@ -99,11 +99,11 @@ class Artist(db.Model):
     __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    image_url = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+    image_url = db.Column(db.String(200))
     country = db.Column(db.String(50))
-    decade = db.Column(db.String(50))
-    genres = db.Column(db.String(100)) #db.Column(db.PickleType(mutable=True)) 
+    decade = db.Column(db.String(100))
+    genre = db.Column(db.String(100)) #db.Column(db.PickleType(mutable=True)) 
 
     # Reference to an artist's most popular track
     # most_popular_track_id = db.Column(db.Integer, db.ForeignKey('tracks.id'))
@@ -140,11 +140,11 @@ class Album(db.Model):
     __tablename__ = 'albums'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    genres = db.Column(db.String(50)) #db.Column(db.String(100))
+    name = db.Column(db.String(150))
+    genre = db.Column(db.String(50)) #db.Column(db.String(100))
     release_date = db.Column(db.String(15))
-    album_cover_url = db.Column(db.String(150))
-    label = db.Column(db.String(50))
+    album_cover_url = db.Column(db.String(200))
+    label = db.Column(db.String(100))
     number_of_tracks = db.Column(db.Integer, nullable=True)
 
     # db.relationship to artist_album_pairs table
@@ -179,8 +179,8 @@ class Concert(db.Model):
     __tablename__ = 'concerts'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    event_link = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+    event_link = db.Column(db.String(200))
     date = db.Column(db.String(15))
     time = db.Column(db.String(10))
 
@@ -212,8 +212,7 @@ class Venue(db.Model):
     __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    image_url = db.Column(db.String(150))
+    name = db.Column(db.String(150))
     city = db.Column(db.String(50))
     region = db.Column(db.String(50))
     country = db.Column(db.String(50))
@@ -224,17 +223,12 @@ class Venue(db.Model):
     # Auto-populates Concert.venue
     concerts = db.relationship('Concert', backref='venue')
 
-    def __init__(self, name, image_url, city, region, country, latitude, longitude):
+    def __init__(self, name, city, region, country, latitude, longitude):
         assert (name != "")
-        assert (image_url != "")
         assert (city != "")
-        assert (region != "")
         assert (country != "")
-        assert (latitude >= 0.0)
-        assert (longitude >= 0.0)
 
         self.name = name
-        self.image_url = image_url
         self.city = city
         self.region = region
         self.country = country
@@ -246,4 +240,6 @@ class Venue(db.Model):
 
 # Create the tables
 db.create_all()
+# db.reflect()
+# db.drop_all()
 db.session.commit()
