d70c3bba296ddd7f2842df6f7e7bf772abb066e7
brodgers <rodgers.benjamin@utexas.edu>
2017-04-05 23:07:25 -0500

    pushed json files for example artists query for frontend testing

--- app/test_db.py
+++ app/test_db.py
@@ -1,14 +1,14 @@
 import json
-from models import db, Artist
+from models import db, Artist, Track
 
 artists = db.session.query(Artist).limit(9).all()
 dicts = {'Artists':[]}
 
-def create_dict(a) :
-	return {'name':a.name, 'image_url':a.image_url, 'country':a.country, 'decade':a.decade, 'genre':a.genre}
+def create_dict(a, t) :
+	return {'name':a.name, 'image_url':a.image_url, 'country':a.country, 'decade':a.decade, 'genre':a.genre, 'most_popular_track_id':t}
 
 for a in artists :
-	dicts['Artists'].append(create_dict(a))
+	dicts['Artists'].append(create_dict(a, a.tracks[-1].id))
 
 
 print(json.dumps(dicts, indent=2))
