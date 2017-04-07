6fe6ab80dd663d7074c7215cf9b97f4c07569289
Nandhini Lakuduva <Nandhini@wireless-10-148-163-188.public.utexas.edu>
2017-04-07 15:31:54 -0500

    updated the makefile to say IDB2 instead

--- app/tests.py
+++ app/tests.py
@@ -312,7 +312,7 @@ class TestCase(unittest.TestCase):
         assert (Album.query.filter_by(name='album_name').first() is None)
         assert (db.session.query(Artist_Album_Association).filter(
             Artist.id == artist_result.id).filter(Album.id == album_result.id).
-                first() is None)
+            first() is None)
 
     def test_db17_concert_aa_relationship(self):
         artist = Artist('artist_name', 'url', 'country', 'decade', 'genre')
@@ -380,12 +380,12 @@ class TestCase(unittest.TestCase):
         assert (db.session.query(Artist_Album_Association).filter(
             Artist_Album_Association.artist_id == artist_result.id).filter(
                 Artist_Album_Association.album_id == album_result.id).first()
-                is None)
+            is None)
         assert (Concert.query.filter_by(name='concert_name').first() is None)
         assert (db.session.query(Concert_AA_Association).filter(
             Concert_AA_Association.aa_id == aa_result.id).filter(
                 Concert_AA_Association.concert_id == concert_result.id).first()
-                is None)
+            is None)
         assert (Artist.query.filter_by(name='artist_name').first() is None)
         assert (Album.query.filter_by(name='album_name').first() is None)
 
