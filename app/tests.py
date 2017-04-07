91365c16cde3235a1eb1e6ab37350af18ac9d6ce
brodgers <rodgers.benjamin@utexas.edu>
2017-04-07 12:47:38 -0500

    uncommented emran's tests in tests.py, failing 9 tests

--- app/tests.py
+++ app/tests.py
@@ -28,101 +28,101 @@ class TestCase(unittest.TestCase):
     # def tearDown(self):
     #     self.testbed.deactivate()
 
-    # # Test to see if going to the main URL actually works
-    # def test_home_redirects(self):
-    #     rv = self.app.get('/')
-    #     assert rv.status == '302 FOUND'
+    # Test to see if going to the main URL actually works
+    def test_home_redirects(self):
+        rv = self.app.get('/')
+        assert rv.status == '302 FOUND'
 
-    # # Tests to see if boswemianrhapsody.me and boswemianrhapsody.me/home.html
-    # # are the same page
-    # def test_home_default(self):
-    #     rv = self.app.get('/')
-    #     rv1 = self.app.get('/home.html')
+    # Tests to see if boswemianrhapsody.me and boswemianrhapsody.me/home.html
+    # are the same page
+    def test_home_default(self):
+        rv = self.app.get('/')
+        rv1 = self.app.get('/home.html')
 
-    #     assert rv.data == rv1.data
+        assert rv.data == rv1.data
 
-    # # Tests to see if routing to about page works
-    # def test_about_redirect(self):
-    #     rv = self.app.get('/about.html')
-    #     assert rv.status == '302 FOUND'
-    #     assert 'Meet BoSWEmian Rhapsody' in rv.data
+    # Tests to see if routing to about page works
+    def test_about_redirect(self):
+        rv = self.app.get('/about.html')
+        assert rv.status == '302 FOUND'
+        assert 'Meet BoSWEmian Rhapsody' in rv.data
 
-    # # Tests to see if all the tables are accessible
-    # def test_table_redirect(self):
-    #     rv = self.app.get('/tracksTable.html')
-    #     assert rv.status == '302 FOUND'
-    #     assert 'Tracks Table' in rv.data
+    # Tests to see if all the tables are accessible
+    def test_table_redirect(self):
+        rv = self.app.get('/tracksTable.html')
+        assert rv.status == '302 FOUND'
+        assert 'Tracks Table' in rv.data
 
-    #     rv = self.app.get('/artistTable.html')
-    #     assert rv.status == '302 FOUND'
-    #     assert 'Artist Table' in rv.data
+        rv = self.app.get('/artistTable.html')
+        assert rv.status == '302 FOUND'
+        assert 'Artist Table' in rv.data
 
-    #     rv = self.app.get('/albumsTable.html')
-    #     assert rv.status == '302 FOUND'
-    #     assert 'Albums Table' in rv.data
+        rv = self.app.get('/albumsTable.html')
+        assert rv.status == '302 FOUND'
+        assert 'Albums Table' in rv.data
 
-    #     rv = self.app.get('/concertsTable.html')
-    #     assert rv.status == '302 FOUND'
-    #     assert 'Concerts Table' in rv.data
+        rv = self.app.get('/concertsTable.html')
+        assert rv.status == '302 FOUND'
+        assert 'Concerts Table' in rv.data
 
-    # # Tests to see if posting a new album to the album table works
-    # def test_post_album(self):
-    #     rv = self.app.post('/albumsTable.html', data=dict(
-    #         name='A Head Full of Dreams'
-    #     ), follow_redirects=True)
-    #     assert 'Added' in rv.data
+    # Tests to see if posting a new album to the album table works
+    def test_post_album(self):
+        rv = self.app.post('/albumsTable.html', data=dict(
+            name='A Head Full of Dreams'
+        ), follow_redirects=True)
+        assert 'Added' in rv.data
 
-    #     rv = self.app.get('/albumsTable.html')
-    #     assert 'No albums' not in rv.data
-    #     assert 'A Head Full of Dreams' in rv.data
+        rv = self.app.get('/albumsTable.html')
+        assert 'No albums' not in rv.data
+        assert 'A Head Full of Dreams' in rv.data
 
-    # # Tests to see if the posted album can be modified
-    # def test_put_album(self):
-    #     test_post_album()
-    #     rv = self.app.put('/albumsTable.html', data=dict(
-    #         id=2000
-    #             name='A Head Full Of Dreams'
-    #     ), follow_redirects=True)
-    #     assert rv.status == '200 SUCCESS'
-    #     assert 'A Head Full Of Dreams' in rv.data
+    # Tests to see if the posted album can be modified
+    def test_put_album(self):
+        test_post_album()
+        rv = self.app.put('/albumsTable.html', data=dict(
+            id=2000,
+                name='A Head Full Of Dreams'
+        ), follow_redirects=True)
+        assert rv.status == '200 SUCCESS'
+        assert 'A Head Full Of Dreams' in rv.data
 
-    # # Tests to see if it is possible to go to the album page after
-    # # it has been posted
-    # def test_album_table_redirect(self):
-    #     test_post_album()
-    #     rv = self.app.get('/coldplayAlbumPage.html')
-    #     assert rv.status == '302 FOUND'
-    #     assert 'A Head Full of Dreams' in rv.data
+    # Tests to see if it is possible to go to the album page after
+    # it has been posted
+    def test_album_table_redirect(self):
+        test_post_album()
+        rv = self.app.get('/coldplayAlbumPage.html')
+        assert rv.status == '302 FOUND'
+        assert 'A Head Full of Dreams' in rv.data
 
-    # # Tests posting a track to the track table.
-    # def test_post_track(self):
-    #     rv = self.app.post('/tracksTable.html', data=dict(
-    #         name='Adventure of a Lifetime',
-    #         id=2000
-    #     ), follow_redirects=True)
-    #     assert 'Added' in rv.data
+    # Tests posting a track to the track table.
+    def test_post_track(self):
+        rv = self.app.post('/tracksTable.html', data=dict(
+            name='Adventure of a Lifetime',
+            id=2000,
+        ), follow_redirects=True)
+        assert 'Added' in rv.data
 
-    #     rv = self.app.get('/tracksTable.html')
-    #     assert 'No tracks' not in rv.data
-    #     assert 'Adventure of a Lifetime' in rv.data
+        rv = self.app.get('/tracksTable.html')
+        assert 'No tracks' not in rv.data
+        assert 'Adventure of a Lifetime' in rv.data
 
-    # # Tests modifying a posted track. Documentation will show that
-    # # this is different from modifying an album since a track requires
-    # # both a name AND an ID, rather than just and ID.
-    # def test_put_track(self):
-    #     rv = self.app.put('/tracksTable.html', data=dict(
-    #         id=2000
-    #             name='Adventure Of A Lifetime'
-    #     ), follow_redirects=True)
+    # Tests modifying a posted track. Documentation will show that
+    # this is different from modifying an album since a track requires
+    # both a name AND an ID, rather than just and ID.
+    def test_put_track(self):
+        rv = self.app.put('/tracksTable.html', data=dict(
+            id=2000,
+                name='Adventure Of A Lifetime'
+        ), follow_redirects=True)
 
-    #     assert rv.status == '200 SUCCESS'
-    #     assert 'Adventure Of A Lifetime' in rv.data
+        assert rv.status == '200 SUCCESS'
+        assert 'Adventure Of A Lifetime' in rv.data
 
-    # # Tests 404 page.
-    # def test_404(self):
-    #     rv = self.app.get('/missing')
-    #     assert rv.status == '404 NOT FOUND'
-    #     assert '<h1>Not found</h1>' in rv.data
+    # Tests 404 page.
+    def test_404(self):
+        rv = self.app.get('/missing')
+        assert rv.status == '404 NOT FOUND'
+        assert '<h1>Not found</h1>' in rv.data
 
     def test_db01_create_track(self):
         track = Track('track_name', 'genre_name', 'release_date', 1, 0, 'url', True, 'uri')
